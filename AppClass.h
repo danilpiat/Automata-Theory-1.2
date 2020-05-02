088ed9b5fe3df1aa5a36d27ba2c4af33a4254a36
danilpiat <56876348+danilpiat@users.noreply.github.com>
2020-05-02 14:45:36 +0600

    Update AppClass.h

--- AppClass.h
+++ AppClass.h
@@ -23,12 +23,10 @@ public:
 	void inc() { ++count; }
 	void clear() { count = 0; }
 	void less(int number);
-	void print(std::string message);
 	void add();
 	std::string get() const { return buf; }
 	void pushHeadline() { type += temp; }
 	void check();
-	void Refresh() { count = 0; isAcceptable = true; }
 	inline void Acceptable() { isAcceptable = true; };
 	inline void Unacceptable() { isAcceptable = false; };
 };
